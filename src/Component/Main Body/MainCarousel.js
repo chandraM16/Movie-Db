739ff0a2f60628cb1f274d7e3333677f5d64e664
chandraM16 <chandanmarghade@gmail.com>
2023-04-02 20:35:30 +0530

    corousel Linkd with content showcases

--- src/Component/Main Body/MainCarousel.js
+++ src/Component/Main Body/MainCarousel.js
@@ -1,30 +1,48 @@
 import React from "react";
 import { sampleObj as sampleObj } from "../Movies/smapleMovies";
-
+import { Link } from "react-router-dom";
 export const MainCarousel = () => {
   return (
     <div id="carouselExampleFade" className="carousel slide carousel-fade">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img
-            src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[0].backdrop_path}`}
-            className="d-block w-100 carousel-img"
-            alt="..."
-          />
+          <Link
+            to={`/movie/popular/0/${sampleObj[0].id}`}
+            style={{ textDecoration: "none" }}
+            state={{ ...sampleObj[0] }}
+          >
+            <img
+              src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[0].backdrop_path}`}
+              className="d-block w-100 carousel-img"
+              alt="..."
+            />
+          </Link>
         </div>
         <div className="carousel-item ">
-          <img
-            src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[1].backdrop_path}`}
-            className="d-block w-100 carousel-img"
-            alt="..."
-          />
+          <Link
+            to={`/movie/popular/0/${sampleObj[1].id}`}
+            style={{ textDecoration: "none" }}
+            state={{ ...sampleObj[1] }}
+          >
+            <img
+              src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[1].backdrop_path}`}
+              className="d-block w-100 carousel-img"
+              alt="..."
+            />
+          </Link>
         </div>
         <div className="carousel-item ">
-          <img
-            src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[2].backdrop_path}`}
-            className="d-block w-100 carousel-img"
-            alt="..."
-          />
+          <Link
+            to={`/movie/popular/0/${sampleObj[2].id}`}
+            style={{ textDecoration: "none" }}
+            state={{ ...sampleObj[2] }}
+          >
+            <img
+              src={`https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${sampleObj[2].backdrop_path}`}
+              className="d-block w-100 carousel-img"
+              alt="..."
+            />
+          </Link>
         </div>
       </div>
       <button
